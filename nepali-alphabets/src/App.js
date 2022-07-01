245a5d7a7d8606b0f3e634194c6bfa90b4a269df
SushantChhetry <60019004+SushantChhetry@users.noreply.github.com>
2022-07-01 09:23:05 -0500

    Add: homepage CSS and react-router-dom dependency

--- nepali-alphabets/src/App.js
+++ nepali-alphabets/src/App.js
@@ -1,9 +1,11 @@
-import './App.css';
+import "./App.css";
+import Home from "./Pages/Home";
+import { Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      
+      <Home />
     </div>
   );
 }
